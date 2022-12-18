86a7e83b91ce75facc2006946048fd528c0d8dd4
426Tunn <mac@426tunns-Mac.lan>
2022-12-18 20:14:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+
+app.use('/articles', articleRouter)
+
+
+app.get('/', (req, res)=> {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: Date.now(),
+        description: 'Test description'
+    }]
+    res.render('index', {articles: articles})
+})
+
+
+ 
+
+module.exports = app
---
+++ routes/articles.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+
+const router = express.Router()
+
+router.get('/article' , (req, res)=> {
+    res.send()
+})
+
+
+
+
+module.exports = router
